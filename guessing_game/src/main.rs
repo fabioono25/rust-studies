7de18c7cf39cee7afe916aaebe1d4d4b2fca6f88
fabioono25 <fabioono25@gmail.com>
2023-10-11 12:13:20 +0200

    finish guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,6 @@
+use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     let sparkle_heart = vec![240, 159, 146, 150];
@@ -6,14 +8,34 @@ fn main() {
 
     println!("Guess the number game {sparkle_heart}!");
 
-    print!("Please input your guess: ");
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=101);
+    println!("The secret number is: {secret_number}");
 
-    // creating a mutable variable
-    let mut guess = String::new();
+    loop {
+        print!("Please input your guess: ");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // creating a mutable variable
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // let guess: i32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
