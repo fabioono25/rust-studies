10993eb631773273a37421382eae5936ab6b01f8
fabioono25 <fabioono25@gmail.com>
2023-10-13 09:02:22 +0200

    working with flows

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -24,4 +24,41 @@ fn main() {
 
     let test = 12.3 / 4.9;
     println!("the value is {test}");
+
+    let x = another_function(123);
+    println!("the value is {x}");
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("the value is {number}");
+
+    // working with counter
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 2 {
+            break counter * 2;
+        }
+    };
+
+    println!("the value is {result}");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is {}", a[index]);
+
+        index += 1;
+    }
+
+    println!("the value is {} ", a[0]);
+}
+
+fn another_function(x: i32) -> i32 {
+    println!("Another function: {x}.");
+
+    45
 }
