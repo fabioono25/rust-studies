6b75d972bac8d325d5c9c58a541a8491f071d214
fabioono25 <fabioono25@gmail.com>
2023-10-21 12:15:11 +0200

    working with vectors

--- restaurant/src/main.rs
+++ restaurant/src/main.rs
@@ -22,6 +22,44 @@ fn main() {
     // consume the eat_at_restaurant function, present in the lib.rs file
     restaurant::eat_at_restaurant();
 
+    // working with collections
+    let mut v: Vec<i32> = Vec::new();
+    v.push(4);
+    let v2 = vec![1, 2, 3];
+
+    println!("v is {:?}", v[0]);
+    println!("v2 is {:?}", v2);
+
+    let does_not_exist = v.get(100); // None
+                                     // let does_not_exist2 = &v[100]; // panicking
+
+    // let mut v = vec![1, 2, 3, 4, 5];
+    // let first = &v[0];
+    // v.push(6); // cannot borrow `v` as mutable because it is also borrowed as immutable mutable borrow occurs here
+    // println!("The first element is: {first}");
+
+    let mut v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    for i in &mut v {
+        *i += 50; // dereference operator
+        println!("{}", i);
+    }
+
+    // using enums to store different types
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(3.14),
+        SpreadsheetCell::Text(String::from("blue")),
+    ];
 }
 
 // fn function1() -> fmt::Result {
