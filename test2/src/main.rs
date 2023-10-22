e72a493e75d1feb632696f4aa74a9937bcaa9093
fabioono25 <fabioono25@gmail.com>
2023-10-22 20:01:36 +0200

    using Result Error

--- test2/src/main.rs
+++ test2/src/main.rs
@@ -1,6 +1,46 @@
+use std::{fs::File, io::ErrorKind};
+
 fn main() {
     // panic!("crash and burn");
 
     // let v = vec![1, 2, 3];
     // v[99];
+
+    // let greeting_file_result = File::open("hello.txt");
+    // let greeting_file_result = File::open("hello.txt").unwrap(); // panic!
+    let greeting_file_result = File::open("hello.txt").expect("Failed to open hello.txt"); // panic!
+
+    // panic if error
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => {
+    //         panic!("Problem opening the file: {:?}", error);
+    //     }
+    // };
+
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error)
+    //         }
+    //     }
+    // };
+
+    // using closures
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => File::create("hello.txt").unwrap_or_else(|error| {
+    //             panic!("Problem creating the file: {:?}", error);
+    //         }),
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
 }
