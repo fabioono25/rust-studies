0d592c7202bfc27d377500e8edd33be944e1eb38
fabioono25 <fabioono25@gmail.com>
2023-10-22 20:29:27 +0200

    chaining methods error

--- test2/src/main.rs
+++ test2/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::ErrorKind};
+use std::{fs::File, io::ErrorKind, io::{self, Read}, error::Error};
 
 fn main() {
     // panic!("crash and burn");
@@ -8,7 +8,7 @@ fn main() {
 
     // let greeting_file_result = File::open("hello.txt");
     // let greeting_file_result = File::open("hello.txt").unwrap(); // panic!
-    let greeting_file_result = File::open("hello.txt").expect("Failed to open hello.txt"); // panic!
+    // let greeting_file_result = File::open("hello.txt").expect("Failed to open hello.txt"); // panic!
 
     // panic if error
     // let greeting_file = match greeting_file_result {
@@ -43,4 +43,57 @@ fn main() {
     //         }
     //     },
     // };
+
+    // let x = read_username_from_file();
+
+    // fs::read_to_string("hello.txt");
+
+    let y = read_username_from_file2();
+    println!("Value of y: {:?}", y);
+
+    let x = last_char_of_first_line("");
+    println!("Value of x: {:?}", x);
+
+    let z = read_file();
+    println!("Value of z: {:?}", z);
+}
+
+
+fn read_username_from_file () -> Result<String, io::Error> {
+    let user_file_result = File::open("hello.txt");
+
+    let mut username_file = match user_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2 () -> Result<String, io::Error> {
+    // let mut username_file = File::open("hello.txt")?;
+    // let mut username = String::new();
+    // username_file.read_to_string(&mut username)?;
+    // Ok(username)
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
+
+// any kind of error
+fn read_file() -> Result<(), Box<dyn Error>> {
+    let f = File::open("hello.txt")?;
+
+    Ok(())
 }
