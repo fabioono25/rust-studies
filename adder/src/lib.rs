d3e1c5d47de0f0ecbec00ccfd0c6e57814070254
fabioono25 <fabioono25@gmail.com>
2023-10-25 10:31:19 +0200

    finish unit tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,7 +10,14 @@ pub fn greeting(name: &str) -> String {
     format!("Hello {}!", name)
 }
 
-#[cfg(test)]
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
+
+
+#[cfg(test)] // test only when you run cargo test
 mod tests {
     use super::*;
 
@@ -20,10 +27,11 @@ mod tests {
         assert_eq!(result, 4);
     }
 
-    // #[test]
-    // fn another() {
-    //     assert_eq!(add(2, 2), 5);
-    // }
+    #[test]
+    #[ignore]
+    fn another() {
+        assert_eq!(add(2, 2), 5);
+    }
 
     #[test]
     fn larger_can_hold_smaller() {
@@ -72,6 +80,27 @@ mod tests {
     #[should_panic(expected = "Guess value must be between 1 and 100, got 200.")]
     fn greater_than_100() {
         Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
 
