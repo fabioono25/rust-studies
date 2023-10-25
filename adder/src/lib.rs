e99d56ed3c5ba3c7dfbeedd3e762a6b06ac29ab1
fabioono25 <fabioono25@gmail.com>
2023-10-25 09:50:41 +0200

    basic assertions

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,4 +24,43 @@ mod tests {
     // fn another() {
     //     assert_eq!(add(2, 2), 5);
     // }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {width: 8, height: 7};
+        let smaller = Rectangle {width: 5, height: 1};
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {width: 8, height: 7};
+        let smaller = Rectangle {width: 5, height: 1};
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carols"), "Greeting did not contain name, value was `{}`", result);
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) ->bool {
+        self.width > other.width && self.height > other.height
+    }
 }
