3698c7fef99794cfd3a66344716fb55cef08558c
fabioono25 <fabioono25@gmail.com>
2023-10-26 10:04:21 +0200

    error treatment

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,42 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    // let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("{:?}", args);
-    println!("Searching for: {}", query);
-    println!("In file: {}", file_path);
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should habe been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should habe been able to read the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    // Our error values will always be string literals that have the 'static lifetime.
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
