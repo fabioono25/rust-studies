b55075ff4359abe277a2bfb61e708d1eb8005bc7
fabioono25 <fabioono25@gmail.com>
2023-10-27 08:40:51 +0200

    using run and treating errors

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,24 +1,38 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // let config = Config::new(&args);
+    // // let config = Config::new(&args);
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("{:?}", args);
+    // println!("{:?}", args);
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should habe been able to read the file");
+    // let contents =
+    //     fs::read_to_string(config.file_path).expect("Should habe been able to read the file");
 
-    println!("With text:\n{}", contents);
+    // println!("With text:\n{}", contents);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
@@ -29,7 +43,6 @@ struct Config {
 impl Config {
     // Our error values will always be string literals that have the 'static lifetime.
     fn build(args: &[String]) -> Result<Config, &'static str> {
-
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
