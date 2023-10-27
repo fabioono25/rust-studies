fb45f9d823ea278ead80586eb35253171b259733
fabioono25 <fabioono25@gmail.com>
2023-10-27 08:59:17 +0200

    using TDD to add the search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -2,8 +2,8 @@ use std::error::Error;
 use std::fs;
 
 pub struct Config {
-  pub query: String,
-  pub file_path: String,
+    pub query: String,
+    pub file_path: String,
 }
 
 impl Config {
@@ -23,7 +23,40 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+      println!("{}", line)
+    }
 
     Ok(())
+}
+
+// with explicit lifetime 'a we are saying that the returned vector should contain string slices
+// that reference slices of the argument contents (rather than the argument query).
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "love";
+        let contents = "\
+just a test
+with some text
+I love you
+coisa linda";
+
+        assert_eq!(vec!["I love you"], search(query, contents));
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,20 +7,13 @@ use minigrep::run;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // // let config = Config::new(&args);
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    // println!("{:?}", args);
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.file_path);
-
-    // let contents =
-    //     fs::read_to_string(config.file_path).expect("Should habe been able to read the file");
-
-    // println!("With text:\n{}", contents);
 
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
