cb13db323fc9bc8208bda4149aac9bfcbd3d4c40
fabioono25 <fabioono25@gmail.com>
2023-10-31 08:42:11 +0100

    drop smart pointer

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -6,15 +6,32 @@ fn main() {
     println!("b = {}", b);
 
     // working with cons list. Vec<T> is a better choice
-    let _l1 = (1,(2,(3, Nil)));
-    
+    let _l1 = (1, (2, (3, Nil)));
+
     // use the enum List below
     let _l2 = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
-
+    // working with Drop
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
 }
 
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
 }
