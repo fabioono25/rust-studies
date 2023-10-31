4900b4d1030915ea37811dacbfc58cc90c5cb8c6
fabioono25 <fabioono25@gmail.com>
2023-10-31 09:10:19 +0100

    RefCell smart pointer

--- pointers/src/lib.rs
+++ pointers/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::{ops::Deref, cell::RefCell};
 
 #[cfg(test)] // test only when you run cargo test
 mod tests {
@@ -22,13 +22,21 @@ mod tests {
         assert_eq!(5, x);
         assert_eq!(5, *y);
     }
-}
 
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
 
 struct MyBox<T>(T); // tuple struct
 
 impl<T> Deref for MyBox<T> {
-
     type Target = T; // associated type
 
     fn deref(&self) -> &Self::Target {
@@ -38,4 +46,66 @@ impl<T> Deref for MyBox<T> {
     // fn new(x: T) -> MyBox<T> {
     //     MyBox(x)
     // }
+}
+
+struct MockMessenger {
+    sent_messages: RefCell<Vec<String>>,
+}
+
+impl MockMessenger {
+    fn new() -> MockMessenger {
+        MockMessenger {
+            sent_messages: RefCell::new(vec![]),
+        }
+    }
+}
+
+impl Messenger for MockMessenger {
+    fn send(&self, message: &str) {
+
+        let mut one_borrow = self.sent_messages.borrow_mut();
+        let mut two_borrow = self.sent_messages.borrow_mut();
+
+        one_borrow.push(String::from(message));
+        two_borrow.push(String::from(message));
+
+        // self.sent_messages.borrow_mut().push(String::from(message));
+    }
+}
+
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger.send("Urgent: You're at 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger.send("Warning: You're at 75% of your quota!");
+        }
+    }
 }
--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
 // use Nil
 use crate::List::{Cons, Nil};
@@ -22,14 +22,32 @@ fn main() {
     };
     println!("CustomSmartPointers created.");
 
-    let aa = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let bb = Cons(3, Rc::clone(&aa));
-    let cc = Cons(4, Rc::clone(&aa));
-    println!("count after creating bb = {}", Rc::strong_count(&aa));
-    {
-        let dd = Cons(4, Rc::clone(&aa));
-        println!("count after creating dd = {}", Rc::strong_count(&aa));
-    }
+    // working with Rc<T>
+    // let aa = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // let bb = Cons(3, Rc::clone(&aa));
+    // let cc = Cons(4, Rc::clone(&aa));
+    // println!("count after creating bb = {}", Rc::strong_count(&aa));
+    // {
+    //     let dd = Cons(4, Rc::clone(&aa));
+    //     println!("count after creating dd = {}", Rc::strong_count(&aa));
+    // }
+
+    // working with RefCell<T>
+    // let x = 5;
+    // let y = &mut x; // cannot borrow `x` as mutable, as it is not declared as mutable
+
+    // let value = Rc::new(RefCell::new(5));
+
+    // let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    // let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    // let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    // *value.borrow_mut() += 10;
+
+    // println!("a after = {:?}", a);
+    // println!("b after = {:?}", b);
+    // println!("c after = {:?}", c);
 }
 
 // enum List {
@@ -37,8 +55,12 @@ fn main() {
 //     Nil,
 // }
 
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
