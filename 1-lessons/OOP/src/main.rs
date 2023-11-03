48cd3d09079c53beb35dbef0fb6dcf68f7695f8a
fabioono25 <fabioono25@gmail.com>
2023-11-03 09:00:04 +0100

    example of implementation

--- 1-lessons/OOP/src/main.rs
+++ 1-lessons/OOP/src/main.rs
@@ -1,36 +1,76 @@
+extern crate OOP;
+
+use OOP::{Draw, Button, Screen};
+
 fn main() {
-    println!("Hello, world!");
+
+    // let screen = Screen {
+    //     components: vec![Box::new(String::from("Hi"))],
+    // };
+
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
 }
 
-pub struct AverageCollection {
-    list: Vec<i32>,
-    average: f64,
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
 }
 
-// implementation of public methods
-impl AveragedCollection {
-    pub fn add(&mut self, value: i32) {
-        self.list.push(value);
-        self.update_average();
-    }
-
-    pub fn remove(&mut self) -> Option<i32> {
-        let result = self.list.pop();
-        match result {
-            Some(value) => {
-                self.update_average();
-                Some(value)
-            }
-            None => None,
-        }
-    }
-
-    pub fn average(&self) -> f64 {
-        self.average
-    }
-
-    fn update_average(&mut self) {
-        let total: i32 = self.list.iter().sum();
-        self.average = total as f64 / self.list.len() as f64;
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
     }
 }
+
+// pub struct AverageCollection {
+//     list: Vec<i32>,
+//     average: f64,
+// }
+
+// // implementation of public methods
+// impl AveragedCollection {
+//     pub fn add(&mut self, value: i32) {
+//         self.list.push(value);
+//         self.update_average();
+//     }
+
+//     pub fn remove(&mut self) -> Option<i32> {
+//         let result = self.list.pop();
+//         match result {
+//             Some(value) => {
+//                 self.update_average();
+//                 Some(value)
+//             }
+//             None => None,
+//         }
+//     }
+
+//     pub fn average(&self) -> f64 {
+//         self.average
+//     }
+
+//     fn update_average(&mut self) {
+//         let total: i32 = self.list.iter().sum();
+//         self.average = total as f64 / self.list.len() as f64;
+//     }
+// }
